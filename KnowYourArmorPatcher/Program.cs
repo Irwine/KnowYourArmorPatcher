3f993fd35acf05756f0c6bdf6ed161a286d6f5aa
Irwine <vandevelde632@gmail.com>
2021-08-29 20:28:41 +0200

    Update Program.cs

--- KnowYourArmorPatcher/Program.cs
+++ KnowYourArmorPatcher/Program.cs
@@ -164,8 +164,8 @@ namespace KnowYourArmorPatcher
                 }
             }
             //return description.ToString();
-            //return Encoding.GetEncoding("ISO-8859-1").GetString(Encoding.UTF8.GetBytes(description.ToString()));
-            return Encoding.GetEncoding("UTF8").GetString.GetBytes(description.ToString());
+            return Encoding.GetEncoding("ISO-8859-1").GetString(Encoding.UTF8.GetBytes(description.ToString()));
+            
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
