1ba18f5cd1f90f8eed64e4c79c6e2b4cbcfc74a3
Selphira <80216629+Selphira@users.noreply.github.com>
2021-12-27 16:42:27 +0100

    Update Settings.cs

--- KnowYourArmorPatcher/Settings.cs
+++ KnowYourArmorPatcher/Settings.cs
@@ -1,7 +1,10 @@
-﻿namespace KnowYourArmorPatcher
+using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace KnowYourArmorPatcher
 {
     public class Settings
     {
+        [SettingName("Intensité de l'effet")]
         public float EffectIntensity { get; set; } = 1.0f;
         //public bool PatchArmorDescriptions { get; set; } = true;
     }
